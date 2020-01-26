c8a51e3ceb7ea94ea05c3b5e5d80c530a180a5ff
Nathaniel-github <55164518+Nathaniel-github@users.noreply.github.com>
2020-01-26 13:39:49 -0800

    Updates BIIG
    FINISHED
    All of gen 1 is now updated to the text file. Although they dont have sats or moves yet all the mons are there;

--- PokemonInfo.java
+++ PokemonInfo.java
@@ -6,12 +6,8 @@ public class PokemonInfo {
 	public String getInfo(String segment, int dex) {
 		String answer = "";
 		int ndex = myTranslator.getIntForDexNum(dex);
-		if (ndex == -1) { //-1 is the return value if the input value is not an accepted value
-			return "That was not a valid dex number";
-		}
 		int num = myTranslator.getIntForSegment(segment);
 		answer = kleb.getPokemonInfo(num, ndex);
 		return answer;
 	}
-	
 }
